c98c0677ac1696340e5a42796790e90520f3cbc3
Marmington111 <marmington111@gmail.com>
2015-01-21 18:52:32 -0500

    Intro Section for Homepage
    - Intro Section added to front-page.php
    - Style for the intro section were added

--- wp-content/themes/bms/front-page.php
+++ wp-content/themes/bms/front-page.php
@@ -12,12 +12,26 @@
 
 get_header(); ?>
 
-    <div class="inner-wrap">
-		<div id="primary" class="content-area">
-            <main id="main" class="site-main" role="main">
-               
-            </main><!-- #main -->
-		</div><!-- #primary -->
-    </div><!-- inner-wrap -->
+	<div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+        	<section class="site-intro">
+        		<div class="inner-wrap">
+	           		<h1 class="section-title star">
+	           			<span>
+		           			The First of its Kind
+
+		           			<i></i>
+	           			</span>
+	           		</h1>
+
+	           		<h2 class="section-content">
+	           			A revolutionary approach to <strong><em>injury rehabilitation</em></strong>. 
+	           			The nervous system is amazingly plastic and adaptable. More <strong><em>efficient movement patterns</em></strong> 
+	           			are the key to <strong><em>enjoying</em></strong> your favorite activities, sports, and <strong><em>life</em></strong>.
+	           		</h2>
+           		</div><!-- inner-wrap -->
+        	</section><!-- site-intro -->
+        </main><!-- #main -->
+	</div><!-- #primary -->
 
 <?php get_footer(); ?>
