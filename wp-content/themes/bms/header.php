3838b5d8f9e9ca9988a2e9786cda2a959d3f2229
Marmington111 <marmington111@gmail.com>
2015-01-24 01:39:18 -0500

    Minor Style Update
    blah

--- wp-content/themes/bms/header.php
+++ wp-content/themes/bms/header.php
@@ -58,7 +58,7 @@
 	
 
 			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<div class="inner-wrap">
+				<div class="inner-wrap clearfix">
 					<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bms' ); ?></button>
 					<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
 
