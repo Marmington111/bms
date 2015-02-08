ade6a6f424df80838f81f991883b9cf3ba4aa88f
Marmington111 <marmington111@gmail.com>
2015-02-08 10:31:33 -0500

    About Page Update
    - Meta boxes added and page completed

--- wp-content/themes/bms/content-about.php
+++ wp-content/themes/bms/content-about.php
@@ -8,38 +8,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-	<?php
-		$args = array( 
-			'post_type'			=> 'bio', 
-			'post_status'		=> 'publish',
-		);
-
-		$bio = new WP_Query( $args );
-		if ( $bio->have_posts() ) {
-	?>		
-	    <div class="bio-header clearfix">
-		    <div class="bio-pic">
-			    <?php the_post_thumbnail('bio-thumb'); ?>
-		    </div>
-
-		    <div class="bio-intro">
-			    <div class="bio-name">
-				    <?php the_title(); ?>
-			    </div>'
-			    <div class="bio-blurb">
-				    <?php the_content(); ?>
-			    </div>
-			    <p class="bio-cat"><i class="fa fa-star"></i><span>Position:</span>
-				    <?php echo rwmb_meta( 'rw_position' ); ?>
-			    </p>
-			    <p class="bio-cat"><i class="fa fa-university"></i><span>Experience:</span> 
-				    <?php echo rwmb_meta( 'rw_experience' ); ?>
-			    </p>
-			    <p class="bio-cat"><i class="fa fa-thumbs-up"></i><span>Qualifications:</span>
-				    <?php echo rwmb_meta( 'rw_qualify' ); ?>
-			    </p>
-		    </div>
-		</div>
+	
 	</header><!-- .entry-header -->
 
 	<div class="entry-content span-twelve">
@@ -56,4 +25,3 @@
 		<?php edit_post_link( __( 'Edit', 'bms' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
-<?php } ?>
--- wp-content/themes/bms/page-about.php
+++ wp-content/themes/bms/page-about.php
@@ -9,6 +9,39 @@ get_header(); ?>
 <div class="inner-wrap clearfix">
 	<div id="primary" class="content-area span-twelve clearfix">
 		<main id="main" class="site-main" role="main">
+			<?php
+			$biography = array( 
+				'post_type'			=> 'bio', 
+				'posts_per_page'	=> 1
+			);
+
+			$loop = new WP_Query( $biography );
+
+			while ( $loop->have_posts() ) : $loop->the_post();
+	        	echo '<div class="bio-header clearfix">';
+					echo '<div class="bio-pic">';
+						the_post_thumbnail('bio-thumb');
+					echo '</div>';// bio-pic
+					echo '<div class="bio-intro">';
+						echo '<div class="bio-name">';
+							the_title();
+						echo '</div>';// bio-name
+						echo '<div class="bio-blurb">';
+							the_content();
+						echo '</div>';// bio-blurb
+						echo '<p class="bio-cat"><i class="fa fa-star"></i><span>Position: </span>';
+							echo rwmb_meta( 'rw_position' );
+						echo '</p>';
+						echo '<p class="bio-cat"><i class="fa fa-university"></i><span>Position: </span>';
+							echo rwmb_meta( 'rw_experience' );
+						echo '</p>';
+						echo '<p class="bio-cat"><i class="fa fa-thumbs-up"></i><span>Position: </span>';
+							echo rwmb_meta( 'rw_qualify' );
+						echo '</p>';
+					echo '</div>';// bio-intro
+				echo '</div>';// bio-header
+			endwhile;
+			?>	
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
