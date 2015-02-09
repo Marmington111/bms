71225e8b799abb4cb1c11433a900e53a4df48638
Marmington111 <marmington111@gmail.com>
2015-02-08 19:27:14 -0500

    Contact Page
    - Added header for contact form

--- wp-content/themes/bms/page-contact.php
+++ wp-content/themes/bms/page-contact.php
@@ -9,7 +9,8 @@ get_header(); ?>
 <div class="inner-wrap clearfix">
 	<div id="primary" class="content-area span-eight clearfix">
 		<main id="main" class="site-main" role="main">
-
+			<h2 class="widget-title">Get in Touch</h2>
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
