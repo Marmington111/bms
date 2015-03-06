97a8b35f17ba6efae7362d964d3c9b0e5ec9ed21
Marmington111 <marmington111@gmail.com>
2015-03-06 09:32:37 -0500

    Copyright
    - Added footer info

--- wp-content/themes/bms/footer.php
+++ wp-content/themes/bms/footer.php
@@ -12,9 +12,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<?php get_sidebar( 'footer' ); ?>
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'bms' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'bms' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://bodymovementsolutions.com/', 'bms' ) ); ?>"><?php printf( __( '&copy; 2015. Body Movement Solutions. All Rights Reserved.', 'bms' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'bms' ), 'bms', '<a href="http://www.marmington.com" rel="designer">Matthew Armington</a>' ); ?>
+			<?php printf( __( 'Theme: Movement by %2$s.', 'bms' ), 'bms', '<a href="http://www.marmington.com" rel="designer">Matthew Armington</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
